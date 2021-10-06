0e032e9015e1036790877df2d212a8e92f8fb2b8
Durbin <jihanislam007@gmail.com>
2021-10-06 17:52:25 +0600

    Class - 16(Drawer)

--- lib/Gridviews.dart
+++ lib/Gridviews.dart
@@ -11,46 +11,154 @@ class Gridviews extends StatelessWidget {
         title: Text("Grid View"),
       ),
       body: GridView.count(
-        crossAxisCount: 4,
+        crossAxisCount: 2,
       crossAxisSpacing: 10,
       mainAxisSpacing: 10,
 
       children: [
 
-        Container(
-          height: 200,
-          width: 200,
-          color: Colors.green,
+        Card(
+          /*shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+
+          ),*/
+          shape: BeveledRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          /*shape: StadiumBorder(
+            side: BorderSide(
+              color: Colors.black,
+              width: 2.0,
+            ),
+          ),*/
+          elevation: 10,
+          child: Container(
+            height: 200,
+            width: 200,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE5Pfpsob_ilAEnMuY4dU5tBZpv2fKngqasEImSYNJp7EKs23tdJJ3xiEIILuGx0iFSoY&usqp=CAU"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Text("Bangladesh"),
+                  )
+
+                ],
+              ),
+            ),
+            //color: Colors.green,
+          ),
         ),
 
-        Container(
-          height: 200,
-          width: 200,
-          color: Colors.green,
+        Card(
+          elevation: 10,
+          child: Container(
+            height: 200,
+            width: 200,
+            color: Colors.green,
+          ),
         ),
 
-        Container(
-          height: 200,
-          width: 200,
-          color: Colors.green,
+        Card(
+          elevation: 10,
+          child: Container(
+            height: 200,
+            width: 200,
+            color: Colors.green,
+          ),
         ),
 
-        Container(
-          height: 200,
-          width: 200,
-          color: Colors.green,
+        Card(
+          /*shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+
+          ),*/
+          shape: BeveledRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          /*shape: StadiumBorder(
+            side: BorderSide(
+              color: Colors.black,
+              width: 2.0,
+            ),
+          ),*/
+          elevation: 10,
+          child: Container(
+            height: 200,
+            width: 200,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE5Pfpsob_ilAEnMuY4dU5tBZpv2fKngqasEImSYNJp7EKs23tdJJ3xiEIILuGx0iFSoY&usqp=CAU"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Text("Bangladesh"),
+                  )
+
+                ],
+              ),
+            ),
+            //color: Colors.green,
+          ),
         ),
 
-        Container(
-          height: 200,
-          width: 200,
-          color: Colors.green,
+        Card(
+          /*shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+
+          ),*/
+          shape: BeveledRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          /*shape: StadiumBorder(
+            side: BorderSide(
+              color: Colors.black,
+              width: 2.0,
+            ),
+          ),*/
+          elevation: 10,
+          child: Container(
+            height: 200,
+            width: 200,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE5Pfpsob_ilAEnMuY4dU5tBZpv2fKngqasEImSYNJp7EKs23tdJJ3xiEIILuGx0iFSoY&usqp=CAU"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Text("Bangladesh"),
+                  )
+
+                ],
+              ),
+            ),
+            //color: Colors.green,
+          ),
         ),
 
-        Container(
-          height: 200,
-          width: 200,
-          color: Colors.green,
+        Card(
+          elevation: 10,
+          child: Container(
+            height: 200,
+            width: 200,
+            color: Colors.green,
+          ),
         ),
 
       ],
--- lib/SecondPage.dart
+++ lib/SecondPage.dart
@@ -16,136 +16,172 @@ import 'ThirdPage.dart';
 class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.agriculture),
-          actions: [Icon(Icons.agriculture)],
-          title: Text("Batch -002"),
-          centerTitle: true,
+    return MaterialApp(
+      home:
+      Scaffold(
+
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  'Drawer Header',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.message),
+                title: Text('Messages'),
+              ),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('Profile'),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+              ),
+            ],
+          ),
         ),
-        body: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: ListView(
-          children: [
-              Padding(
+
+          appBar: AppBar(
+            //leading: Icon(Icons.agriculture),
+            actions: [Icon(Icons.agriculture)],
+            title: Text("Batch -002"),
+            centerTitle: true,
+          ),
+          body: Center(
+              child: Padding(
                 padding: const EdgeInsets.all(18.0),
-                child: Container(
-                    height: 100,
-                    color: Colors.deepOrangeAccent,
-                    child: Center(
-                        child: Text(
-                      "Jomshed Ali",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ))),
-              ),
-              Text(
-                "Jomshed Ali",
-                style: TextStyle(fontSize: 32),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Thirdpage()));
-                  },
-                  child: Text("Button")),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => RowColumn()));
-                  },
-                  child: Text("RowColumn")),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => SingleChildScrollViews()));
-                  },
-                  child: Text("Single Child Scrollview")),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Calculator()));
-                  },
-                  child: Text("Calculator")),
-              Container(
-                child: ElevatedButton(
+                child: ListView(
+            children: [
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Container(
+                      height: 100,
+                      color: Colors.deepOrangeAccent,
+                      child: Center(
+                          child: Text(
+                        "Jomshed Ali",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ))),
+                ),
+                Text(
+                  "Jomshed Ali",
+                  style: TextStyle(fontSize: 32),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Thirdpage()));
+                    },
+                    child: Text("Button")),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => RowColumn()));
+                    },
+                    child: Text("RowColumn")),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SingleChildScrollViews()));
+                    },
+                    child: Text("Single Child Scrollview")),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Calculator()));
+                    },
+                    child: Text("Calculator")),
+                Container(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        print("pressed done");
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => PracticeImage()));
+                      },
+                      child: Text("Image Load")),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      print("pressed done");
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => ListTiles()));
+                    },
+                    child: Text("List Tile")),
+                ElevatedButton(
                     onPressed: () {
                       print("pressed done");
                       Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => PracticeImage()));
+                              builder: (context) => StackPosiotion()));
                     },
-                    child: Text("Image Load")),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    print("pressed done");
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => ListTiles()));
-                  },
-                  child: Text("List Tile")),
-              ElevatedButton(
-                  onPressed: () {
-                    print("pressed done");
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => StackPosiotion()));
-                  },
-                  child: Text("Stack Position")),
-              ElevatedButton(
-                  onPressed: () {
-                    print("pressed done");
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => LiquidSweept()));
-                  },
-                  child: Text("liquid swipe")),
+                    child: Text("Stack Position")),
+                ElevatedButton(
+                    onPressed: () {
+                      print("pressed done");
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LiquidSweept()));
+                    },
+                    child: Text("liquid swipe")),
 
-            ElevatedButton(
-                  onPressed: () {
-                    print("pressed done");
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => Gridviews()));
-                  },
-                  child: Text("Grid view")),
+              ElevatedButton(
+                    onPressed: () {
+                      print("pressed done");
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Gridviews()));
+                    },
+                    child: Text("Grid view")),
 
-            ElevatedButton(
-                  onPressed: () {
-                    print("pressed done");
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => Expandeds()));
-                  },
-                  child: Text("Expanded")),
+              ElevatedButton(
+                    onPressed: () {
+                      print("pressed done");
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Expandeds()));
+                    },
+                    child: Text("Expanded")),
 
-            ElevatedButton(
-                  onPressed: () {
-                    print("pressed done");
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => Cards()));
-                  },
-                  child: Text("Cards")),
-          ],
-        ),
-            )),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.green,
-          onPressed: () {
-            print("I am floating button");
-          },
-        child: Icon(Icons.add,),
-        ),
-        //)
-        );
+              ElevatedButton(
+                    onPressed: () {
+                      print("pressed done");
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Cards()));
+                    },
+                    child: Text("Cards")),
+            ],
+          ),
+              )),
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.green,
+            onPressed: () {
+              print("I am floating button");
+            },
+          child: Icon(Icons.add,),
+          ),
+          //)
+          ),
+    );
   }
 }
